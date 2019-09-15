f9775c0b5e461cc7f845034dc3050dbe96ff7c76
otavio <otavio.resende@estudante.ufla.br>
2019-09-15 18:17:10 -0300

    :rocket: Criação da classe D

---
+++ D.java
@@ -0,0 +1,15 @@
+import static java.lang.System.out;
+
+public class D {
+
+    private String D1;
+    private int D2;
+
+    public void MD1() {
+        System.out.println("Método MD1");
+    }
+
+    public void MD2() {
+        System.out.println("Método MD2");
+    }
+}
