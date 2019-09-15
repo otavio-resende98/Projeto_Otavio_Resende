b162ec36927617f7f0d7769e877c00715f64be3d
Otávio Resende <49536613+otavio-resende98@users.noreply.github.com>
2019-09-15 16:54:07 -0300

    :rocket: Adicionando as classes A e B.
    Classes java que contém métodos get, set, construtor, etc.

---
+++ A.java
@@ -0,0 +1,41 @@
+import static java.lang.System.out;
+
+public class A {
+    
+    private int A1;
+    private float A2;
+
+    public A() {
+        System.out.println("Construtor de A");
+        A1 = 0;
+        A2 = 0;
+    }
+
+    public int getA1() {
+        System.out.println("Método getA1");
+        return A1;
+    }
+
+    public float getA2() {
+        System.out.println("Método getA2");
+        return A2;
+    }
+
+    public void setA1(int A1) {
+        System.out.println("Método setA1");
+        this.A1 = A1;
+    }
+
+    public void setA2(float A2) {
+        System.out.println("Método setA2");
+        this.A2 = A2;
+    }
+
+    public void MA1() {
+        System.out.println("Método MA1");
+    }
+
+    public void MA2() {
+        System.out.println("Método MA2");
+    }
+}
---
+++ B.java
@@ -0,0 +1,41 @@
+import static java.lang.System.out;
+
+public class B {
+    
+    private int B1;
+    private float B2;
+
+    public B() {
+        System.out.println("Construtor de B");
+        B1 = 0;
+        B2 = 0;
+    }
+
+    public int getB1() {
+        System.out.println("Método getB1");
+        return B1;
+    }
+
+    public float getB2() {
+        System.out.println("Método getB2");
+        return B2;
+    }
+
+    public void setB1(int B1) {
+        System.out.println("Método setB1");
+        this.B1 = B1;
+    }
+
+    public void setB2(float B2) {
+        System.out.println("Método setB2");
+        this.B2 = B2;
+    }
+
+    public void MB1() {
+        System.out.println("Método MB1");
+    }
+
+    public void MB2() {
+        System.out.println("Método MB2");
+    }
+}
