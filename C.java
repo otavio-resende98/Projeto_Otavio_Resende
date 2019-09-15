987041ef14fc47d5d71de210683c0099161e60b0
otavio <otavio.resende@estudante.ufla.br>
2019-09-15 17:27:41 -0300

    :rocket: Criação da Classe C.

---
+++ C.java
@@ -0,0 +1,15 @@
+import static java.lang.System.out;
+
+public class C {
+
+    private String C1;
+    private int C2;
+
+    public void MC1() {
+        System.out.println("Método MC1");
+    }
+
+    public void MC2() {
+        System.out.println("Método MC2");
+    }
+}
